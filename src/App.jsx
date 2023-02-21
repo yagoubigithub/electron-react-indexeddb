8b159def23fa53a4c85b1487b27ef7629d36a436
yagoubigithub <yagoubi.aek.2@gmail.com>
2023-02-21 21:28:41 +0100

    put indexeddb

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useLiveQuery } from "dexie-react-hooks";
 
+import { db } from "./db";
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1>My simple Dexie app</h1>
+
+      <h2>Add Friend</h2>
+      <AddFriendForm defaultAge={21} />
+
+      <h2>Friend List</h2>
+      <FriendList minAge={18} maxAge={65} />
+    </div>
+  );
+};
+
+function FriendList({ minAge, maxAge }) {
+  const friends = useLiveQuery(
+    async () => {
+      //
+      // Query Dexie's API
+      //
+      const friends = await db.friends
+        .where("age")
+        .between(minAge, maxAge)
+        .toArray();
+
+      // Return result
+      return friends;
+    },
+    // specify vars that affect query:
+    [minAge, maxAge]
+  );
+
+  return (
+    <ul>
+      {friends?.map((friend) => (
+        <li key={friend.id}>
+          {friend.name}, {friend.age}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+function AddFriendForm({ defaultAge } = { defaultAge: 21 }) {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(defaultAge);
+  const [status, setStatus] = useState("");
+
+  async function addFriend() {
+    try {
+      // Add the new friend!
+      const id = await db.friends.add({
+        name,
+        age,
+      });
+
+      setStatus(`Friend ${name} successfully added. Got id ${id}`);
+      setName("");
+      setAge(defaultAge);
+    } catch (error) {
+      setStatus(`Failed to add ${name}: ${error}`);
+    }
+  }
+
+  return (
+    <>
+      <p>{status}</p>
+      Name:
+      <input
+        type="text"
+        value={name}
+        onChange={(ev) => setName(ev.target.value)}
+      />
+      Age:
+      <input
+        type="number"
+        value={age}
+        onChange={(ev) => setAge(Number(ev.target.value))}
+      />
+      <button onClick={addFriend}>Add</button>
+    </>
+  );
+}
+
+export default App;
